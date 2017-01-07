375a7f9f7af4e792f87396d22c1b481401efc926
Michael Palmer <mpalmer1661@gmail.com>
2017-01-06 22:20:18 -0600

    adding textures

--- include/util.h
+++ include/util.h
@@ -1,6 +1,13 @@
 #pragma once
 
 #include <GL/glew.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "../deps/lodepng/lodepng.h"
+
+#include "error.h"
 
 void load_png_texture(const char *);
+void flip_image_vertical(unsigned char *data, unsigned int width, unsigned int height);
 GLuint load_program(const char *path1, const char *path2);
--- src/util.c
+++ src/util.c
@@ -2,10 +2,38 @@
 
 #include <stdio.h> // just to check test is working!
 
+void flip_image_vertical(unsigned char *data, unsigned int width, unsigned int height)
+{
+	unsigned int size = width * height * 4;
+	unsigned int stride = sizeof(char) * width * 4;
+	unsigned char *new_data = malloc(sizeof(unsigned char) * size);
+
+	for (unsigned int i = 0; i < height; i++) {
+		unsigned int j = height - i - 1;
+		memcpy(new_data + j * stride, data + i * stride, stride);
+	}
+
+	memcpy(data, new_data, size);
+	free(new_data);
+}
 
 void load_png_texture(const char *file) 
 {
 	fprintf(stdout, "load_png_texture() called with %s\n\n", file);
+
+	unsigned int error;
+	unsigned char *data;
+	unsigned int width, height;
+
+	error = lodepng_decode32_file(&data, &width, &height, file);
+
+	if (error)
+		handle_error(900, "lodepng_decode32_file()");
+
+	flip_image_vertical(data, width, height);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+
+	free(data);
 }
 
 GLuint load_program(const char *path1, const char *path2)
